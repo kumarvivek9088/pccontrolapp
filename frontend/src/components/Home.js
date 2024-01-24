663a56bf6a1e7ead283b2b285bf41aee78cb6c08
Vivek Kumar <kumarvivek9088@gmail.com>
2024-01-24 20:22:15 +0530

    added frontend

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,15 +5,25 @@ const Home = ()=>{
     const enablechatdiv = ()=>{
         console.log("chatmenu clicked")
         console.log(!document.getElementsByClassName("botchatbox")[0].style.display);
+        let chatmenuboxdiv = document.getElementsByClassName("menu")[0];
+        let homecontainerdiv = document.getElementsByClassName("homecontainer")[0];
+        console.log(chatmenuboxdiv.offsetWidth ,homecontainerdiv.offsetWidth)
         if(document.getElementsByClassName("botchatbox")[0].style.display === 'none' || !document.getElementsByClassName("botchatbox")[0].style.display){
-            document.getElementsByClassName("botchatbox")[0].style.display='flex';
-            document.getElementsByClassName("botsmenu")[0].style.display = 'none';
-            document.getElementsByClassName("overlaybox")[0].style.display = 'none';
+            if (chatmenuboxdiv.offsetWidth === homecontainerdiv.offsetWidth){
+                document.getElementsByClassName("botchatbox")[0].style.display='flex';
+                document.getElementsByClassName("menu")[0].style.display = 'none';
+                document.getElementsByClassName("overlaybox")[0].style.display = 'none';
+            }else{
+                document.getElementsByClassName("botchatbox")[0].style.display='flex';
+            }
+        }
+        else{
+            document.getElementsByClassName("botchatbox")[0].style.display='none';
         }
     }
     const back = ()=>{
         document.getElementsByClassName("botchatbox")[0].style.display = 'none';
-        document.getElementsByClassName("botsmenu")[0].style.display = 'block';
+        document.getElementsByClassName("menu")[0].style.display = 'flex';
         document.getElementsByClassName("overlaybox")[0].style.display = 'flex';
     }
     const triggerdotmenu = ()=>{
@@ -28,12 +38,10 @@ const Home = ()=>{
     const cancelcreateroom = ()=>{
         document.getElementsByClassName("createroom")[0].style.display = 'none';
         phoneassistantmenu();
-        assistantmenu();
     }
     const createnewroom = ()=>{
         document.getElementsByClassName("createroom")[0].style.display = 'flex';
         phoneassistantmenu();
-        assistantmenu();
     }
     const phoneassistantmenu = ()=>{
         let d = document.getElementsByClassName("overlaymenu")[0];
@@ -53,49 +61,68 @@ const Home = ()=>{
             d.style.display = 'none';
         }
     }
+    const cancelbigscreencreateroom= ()=>{
+        document.getElementsByClassName("createroom")[0].style.display = 'none';
+        assistantmenu();
+    }
     return (
         <>
             <div className="homebox">
                 <div className="mainhomecontainer">
                     <div className="homecontainer">
-                        <div className="botsmenu">
-                            <h1>Bots</h1>
-                            <div className="userbots">
-                                <div className="botpic">
-                                    <li className="material-symbols-outlined chatheaderbotpic">account_circle</li>
-                                    {/* <div className="profilepic">
-
-                                    </div> */}
+                        <div className="menu">
+                            <div className="overlaybox">
+                                <div className="suboverlaybox">
+                                    <li class="material-symbols-outlined overlay-symbol" onClick={phoneassistantmenu}>assistant_navigation</li>
                                 </div>
-                                <div className="botname-lastmessage"onClick={enablechatdiv}>
-                                    <div className="botname"> 
-                                        <p>Jarvis</p>
-                                        <span>12:33PM</span>
+                                <div className="overlaymenu">
+                                    <div className="dotmenuitems" onClick={createnewroom}>
+                                        <p>Create Room</p>
                                     </div>
-                                    <div className="lastmessage">
-                                        <p>this is your order sir</p>
+                                    <div className="dotmenuitems" >
+                                        <p>Logout</p>
                                     </div>
                                 </div>
                             </div>
-                            <div className="userbots">
-                                <div className="botpic">
-                                    <li className="material-symbols-outlined chatheaderbotpic">account_circle</li>
-                                    {/* <div className="profilepic">
-                                            
+                            <div className="botsmenu">
+                                <h1>Bots</h1>
+                                <div className="userbots">
+                                    <div className="botpic">
+                                        <li className="material-symbols-outlined chatheaderbotpic">account_circle</li>
+                                        {/* <div className="profilepic">
+
                                         </div> */}
-                                </div>
-                                <div className="botname-lastmessage">
-                                    <div className="botname">
-                                        <p>Friday</p>                                  
-                                        <span>21/1/2024</span>
                                     </div>
-                                    <div className="lastmessage">
-                                        <p>completed sir</p>
+                                    <div className="botname-lastmessage"onClick={enablechatdiv}>
+                                        <div className="botname"> 
+                                            <p>Jarvis</p>
+                                            <span>12:33PM</span>
+                                        </div>
+                                        <div className="lastmessage">
+                                            <p>this is your order sir</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="userbots">
+                                    <div className="botpic">
+                                        <li className="material-symbols-outlined chatheaderbotpic">account_circle</li>
+                                        {/* <div className="profilepic">
+                                                
+                                            </div> */}
+                                    </div>
+                                    <div className="botname-lastmessage">
+                                        <div className="botname">
+                                            <p>Friday</p>                                  
+                                            <span>21/1/2024</span>
+                                        </div>
+                                        <div className="lastmessage">
+                                            <p>completed sir</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="overlaybox">
+                        {/* <div className="overlaybox">
                             <div className="suboverlaybox">
                                 <li class="material-symbols-outlined overlay-symbol" onClick={phoneassistantmenu}>assistant_navigation</li>
                             </div>
@@ -107,11 +134,11 @@ const Home = ()=>{
                                     <p>Logout</p>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                         <div className="createroom">
                             <div className="subcreateroom">
-                                <span class="material-symbols-outlined" onClick={cancelcreateroom}>chevron_left</span>
-                                {/* <span class="material-symbols-outlined" onClick={cancelcreateroom}>close</span> */}
+                                <span class="material-symbols-outlined mobilescreencreateroomclose" onClick={cancelcreateroom} >chevron_left</span>
+                                <span class="material-symbols-outlined bigscreencreateroomclose" onClick={cancelbigscreencreateroom} >close</span>
                                 <p>create a new room</p>
                                 <input type="text" required placeholder="enter your bot name"></input>
                                 <button>Create</button>
@@ -150,25 +177,33 @@ const Home = ()=>{
                             </div>
                             <div className="messagebox">
                                 <div className="submessagebox">
-                                    <div className="receivemessagebox">
-                                        <div className="receivemessagebackgroundbox">
-                                            <p>Hello boss</p>
+                                    <div className="messagecontainer">
+                                        <div className="receivemessagebox">
+                                            <div className="receivemessagebackgroundbox">
+                                                <p>Hello boss</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="receivemessagebox">
-                                        <div className="receivemessagebackgroundbox">
-                                            <p></p>
+                                        <div className="receivemessagebox">
+                                            <div className="receivemessagebackgroundbox">
+                                                <p>hii...</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="sendmessagebox">
-                                        <div className="sendmessagebackgroundbox">
-                                            <p>Hello</p>
+                                        <div className="sendmessagebox">
+                                            <div className="sendmessagebackgroundbox">
+                                                <p>Hello</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="sendmessagebox">
-                                        <div className="sendmessagebackgroundbox">
-                                            <p>When you develop a mockup page or backend API is not ready for data fetching and you have to make Frontend Development with static data until it comes, react-lorem-ipsum will create your gibberish texts for you.</p>
+                                        <div className="sendmessagebox">
+                                            <div className="sendmessagebackgroundbox">
+                                                <p>When you develop a mockup page or backend API is not ready for data fetching and you have to make Frontend Development with static data until it comes, react-lorem-ipsum will create your gibberish texts for you.</p>
+                                            </div>
                                         </div>
+                                        <div className="receivemessagebox">
+                                            <div className="receivemessagebackgroundbox">
+                                                <p>hii dubara se...</p>
+                                            </div>
+                                        </div>
+
                                     </div>
                                 </div>
                                 <div className="messageinputbox">
