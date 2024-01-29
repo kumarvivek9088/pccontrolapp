5362c9e56ce4e99df8792b2a1bc5a9f87cd6145b
Vivek Kumar <kumarvivek9088@gmail.com>
2024-01-29 23:50:06 +0530

    added frontend url in cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 const port = 3001;
 app.use(express.json());
 app.use(cors());
-var allowlist = ['http://localhost:3000','http://127.0.0.1:3000','http://192.168.146.4:3000','https://pccontrolapp.vercel.app'];
+var allowlist = ['http://localhost:3000','http://127.0.0.1:3000','http://192.168.146.4:3000','https://pccontrolapp.vercel.app','https://pccontrolapp.vercel.app:1'];
 var corsConfig = function (req,callback){
     var corsOptions;
     if (allowlist.indexOf(req.header('Origin')) !== -1){
@@ -44,7 +44,7 @@ app.get('/test',(req,res)=>{
 const server = http.createServer(app);
 const io = new Server(server,{
     cors: {
-        origin : ['http://localhost:3000','http://127.0.0.1:3000','http://192.168.146.4:3000','https://pccontrolapp.vercel.app'],
+        origin : allowlist,
         methods : ['GET','POST'],
     },
 });
